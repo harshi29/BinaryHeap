c7de0e82ba2ec986d8b59397c7e5f8a0bf7603e3
Harshita Rastogi <34446281+harshi29@users.noreply.github.com>
2020-02-11 22:45:09 -0600

    Update BinaryHeap.java

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -1,11 +1,12 @@
-package idsa;
+package hxr190001;
 
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 
 public class BinaryHeap<T extends Comparable<? super T>> {
-    Comparable[] pq;
+    @SuppressWarnings("rawtypes")
+	Comparable[] pq;
     int size;
 
     // Constructor for building an empty priority queue using natural ordering of T
@@ -16,17 +17,23 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     // add method: resize pq if needed
     public boolean add(T x) {
+    	if (this.size == pq.length) {
+            this.resize();	
+        }
+        pq[this.size] = x;
+        percolateUp(this.size);
+        this.size++;
 	return true;
     }
 
     public boolean offer(T x) {
 	return add(x);
     }
-
+    
     // throw exception if pq is empty
     public T remove() throws NoSuchElementException {
 	T result = poll();
-	if(result == null) {
+	if(result == null || isEmpty()) {
 	    throw new NoSuchElementException("Priority queue is empty");
 	} else {
 	    return result;
@@ -35,7 +42,14 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     // return null if pq is empty
     public T poll() {
-	return null;
+    	if (isEmpty())
+    		return null;
+    	T val = null ;
+    	val = (T)pq[0];
+        pq[0] = pq[size-1];
+        size--;
+        percolateDown(0);
+        return val;
     }
     
     public T min() { 
@@ -43,8 +57,11 @@ public class BinaryHeap<T extends Comparable<? super T>> {
     }
 
     // return null if pq is empty
-    public T peek() {
-	return null;
+    @SuppressWarnings("unchecked")
+	public T peek() {
+    	if (isEmpty())
+    		return null;
+    	return (T) pq[0];
     }
 
     int parent(int i) {
@@ -57,10 +74,37 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     /** pq[index] may violate heap order with parent */
     void percolateUp(int index) {
+    	
+     Comparable x = pq[index];
+   	 while(index > 0 && (compare(pq[parent(index)], x) == 1))
+   	 {
+   		 pq[index] = pq[parent(index)];
+   		 index = parent(index);
+   	 }
+   	 pq[index] = x;	
+    	
     }
 
     /** pq[index] may violate heap order with children */
     void percolateDown(int index) {
+     int left = leftChild(index);
+   	 int right = leftChild(index) + 1 ;
+   	 int smallest = index;
+   	 if(left <size && compare(pq[left], pq[index]) == -1)
+   	 {
+   		 smallest = left;
+   	 }
+   	 if(right <size && compare(pq[right], pq[smallest]) == -1)
+   	 {
+   		 smallest = right;
+   	 }
+   	 if(index != smallest)
+   	 {
+   		 Comparable temp = pq[smallest];
+   		 pq[smallest] = pq[index];
+   		 pq[index] = temp;
+   		 percolateDown(smallest);
+   	 }	
     }
 
 	/** use this whenever an element moved/stored in heap. Will be overridden by IndexedHeap */
@@ -89,6 +133,10 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 
     // Resize array to double the current size
     void resize() {
+    	Comparable[] temp = new Comparable[size*2];
+    	for (int i=0; i<pq.length; i++)
+    		temp[i] = pq[i];
+    	pq = temp;
     }
     
     public interface Index {
@@ -122,7 +170,7 @@ public class BinaryHeap<T extends Comparable<? super T>> {
 	    System.out.print(" " + x);
 	}
 	System.out.println();
-
+	
 	for(int i=0; i<arr.length; i++) {
 	    arr[i] = h.poll();
 	}
